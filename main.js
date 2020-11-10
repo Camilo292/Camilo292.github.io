15689ef4b3d799e7e0d16fa30226ed4fa9c0c172
Camilo292 <caminore22@gmail.com>
2020-11-09 19:20:29 -0500

    sd

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// darkTheme
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -12,3 +13,17 @@ themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme)
   localStorage.setItem('selected-theme', getCurrentTheme())
 })
+// hamburger nav
+const mainMenu = document.getElementById('nav_links')
+const openMenu = document.getElementById('openMenu')
+const closeMenu = document.getElementById('closeMenu')
+function show(){
+  mainMenu.style.display = 'flex'
+  mainMenu.style.top = '0'
+}
+function close(){
+  mainMenu.style.top = '-100%'
+}
+openMenu.addEventListener('click',show)
+closeMenu.addEventListener('click',close)
+mainMenu.addEventListener('click',close)
